ff9862cca8a8f6aaad43016e6b0449cec02e5820
Eduardo <57434811+NoobToDaMaximum@users.noreply.github.com>
2020-11-29 16:46:14 +0100

    Update Jugador.java

--- ProyectoPOO/src/Jugador.java
+++ ProyectoPOO/src/Jugador.java
@@ -1,6 +1,13 @@
 
 public class Jugador extends Personaje{
+	private String nombreJugador;
 	   public Jugador(String nombre, Objeto objeto, Creencia creencia,Localizacion localizacion){
 	        super(nombre, objeto, creencia,localizacion);
 	    }
+	public String getNombreJugador() {
+		return nombreJugador;
+	}
+	public void setNombreJugador(String nombreJugador) {
+		this.nombreJugador = nombreJugador;
+	}
 }
