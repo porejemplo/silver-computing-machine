01e5f7c44bf191511d9b3fedb6d95d6f6038df68
Fernando <ejemplo696@gmail.com>
2020-12-29 14:17:04 +0100

    Anexo 1
    Lectura completa de anexo 1, guardado de datos y modificacion de contructroes encesarios

--- ProyectoPOO/src/NPCs.java
+++ ProyectoPOO/src/NPCs.java
@@ -4,4 +4,7 @@ public abstract class NPCs extends Personaje{
 	   public NPCs(String nombre, Objeto objeto, Creencias creencia,Localizacion localizacion, Ubicacion objetivo){
 	        super(nombre, objeto, creencia,localizacion,objetivo);
 	    }
+	   public NPCs(String nombre, Localizacion localizacion) {
+		   super(nombre, localizacion);
+	   }
 }
