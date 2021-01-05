84f5b03355d2b61451ea6fe7f9b25f2e8ba26d50
Fernando <ejemplo696@gmail.com>
2021-01-05 12:00:17 +0100

    Gestor de Archivos
    Comentarios Gestor de archivos

--- ProyectoPOO/src/GestorArchivos.java
+++ ProyectoPOO/src/GestorArchivos.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class GestorArchivos {
 	private File anexo1;
     private File anexo2;
-    private String nombreArchivo;
     
     // Formato contra el que se van a comparar las lineas de los anexos.
     final String regEncabezado = "<Localizaciones>|<Personajes>|<Objetos>|<Localización Personajes>|<Posesión Objetos>";
@@ -13,10 +12,11 @@ public class GestorArchivos {
     final String regContenidoAnexoII = "^[\\wáéíóú+\s*]+(\\([\\wáéíóú+\s*]+\\))?";
     
     // Utiles
-	private int nLinea = 0; // Se va a utilizar para guardar la linea que se esta utilizando e indicarlo en caso de error.
+	private String nombreArchivo; // Se utiliza para guardar el nombre del archivo que se esta utilizando para referenciarlo en los errores.
+    private int nLinea = 0; // Se va a utilizar para guardar la linea que se esta utilizando e indicarlo en caso de error.
 	private int selector = 0; 
 	
-	// Constructor.    
+	// Constructores.    
     public GestorArchivos(){};
     public GestorArchivos(File fAnexoI, File fAnexoII) {
         this.anexo1 = fAnexoI;
@@ -49,18 +49,18 @@ public class GestorArchivos {
     // Funciones
     public void comprobarFormato() throws FileNotFoundException, GestorArchivosException
     {
-    	Scanner s = new Scanner (anexo1);
-        nombreArchivo = anexo1.getName();
-        comprobarFormatoAnexoI(s);
-        
-        s = new Scanner (anexo2);
-        nombreArchivo = anexo2.getName();
-        comprobarFormatoAnexoII(s);
+		Scanner s = new Scanner (anexo1);	//Primero leemos el anexo 1
+		nombreArchivo = anexo1.getName();	//luego se asigna el nombre del archivo para errores
+        comprobarFormatoAnexoI(s);			//y por ultimo se comprueba el anexo 1.
+		
+		s = new Scanner (anexo2);			//Primero leemos el anexo 2
+		nombreArchivo = anexo2.getName();	//luego se asigna el nombre del archivo para errores
+        comprobarFormatoAnexoII(s);			//por ultimo se comprueba el anexo 2.
         
         s.close();
     }
     
-    // Funcion para saber el tamaño de cada lista
+    // Funcion para saber el tamaño de cada lista(0-Localizaciones, 1-Personajes, 2-Objetos)
     public int tamanoLista(int seleccion) throws FileNotFoundException
     {
     	selector = -1;
@@ -87,7 +87,8 @@ public class GestorArchivos {
         
         return contador;
     }
-    
+	
+	// Lee los anexos y asigna los valores a las que se le pasan.
     public void leerAnexos(Localizacion lLocalizacion[], Personaje lPersonaje[], Objeto lObjeto[]) throws FileNotFoundException, GestorArchivosException
     {
     	// Comenzamos con el anexoI
@@ -102,8 +103,6 @@ public class GestorArchivos {
         // Sacamos los datos de los objetivos del anexoII
         s = new Scanner (anexo2);
         nombreArchivo = anexo2.getName();
-        //comprobarFormatoAnexoII(s);
-        //s = new Scanner (anexo2);
         darValoresAnexoII(s, lPersonaje);
 
         s.close();
@@ -115,12 +114,12 @@ public class GestorArchivos {
         nLinea = 0;
         while (s.hasNextLine()) {
             ++nLinea;
-            String linea = s.nextLine();
-            if(linea.charAt(0) == '<' || nLinea==1){ //Comprueba si la linea es un encabezado o si es la primera del documento.
-                if (!linea.matches(regEncabezado))
+			String linea = s.nextLine();
+            if(linea.charAt(0) == '<' || nLinea==1){ 		// Si es la priera linea del documento o empieza por <
+				if (!linea.matches(regEncabezado)) 			// se compara el formato de la linea con el de la cabecera.
                     throw new GestorArchivosException(nLinea,nombreArchivo,"Formato");
             }
-            else if (!linea.matches(regContenidoAnexoI)){ // Si no es un encabezado se mira si el formato es de contenido.
+            else if (!linea.matches(regContenidoAnexoI)){ 	// Si no es un encabezado se mira si el formato es de contenido.
             	throw new GestorArchivosException(nLinea,nombreArchivo,"Formato");
             }
         }
@@ -133,17 +132,17 @@ public class GestorArchivos {
         while (s.hasNextLine()) {
             ++nLinea;
             String linea = s.nextLine();
-            if(linea.charAt(0) == '<' || nLinea==1){ //Comprueba si la linea es un encabezado o si es la primera del documento.
-                if (!linea.matches(regEncabezado))
+            if(linea.charAt(0) == '<' || nLinea==1){ 		// Si es la priera linea del documento o empieza por <
+				if (!linea.matches(regEncabezado)) 			// se compara el formato de la linea con el de la cabecera.
                 	throw new GestorArchivosException(nLinea,nombreArchivo,"Formato");
             }
-            else if (!linea.matches(regContenidoAnexoII)){// Si no es un encabezado se mira si el formato es de contenido.
+            else if (!linea.matches(regContenidoAnexoII)){	// Si no es un encabezado se mira si el formato es de contenido.
             	throw new GestorArchivosException(nLinea,nombreArchivo,"Formato");
             }
         }
     }
     
-    // Lee el documento y comienza a guardar valores en las listas
+    // Lee el AnexoI y comienza a guardar valores en las listas
     private void darValorListasAnexoI(Scanner s, Localizacion lLocalizacion[], Personaje lPersonaje[], Objeto lObjeto[]) throws GestorArchivosException
     {
     	nLinea = 0;
@@ -175,48 +174,59 @@ public class GestorArchivos {
             }
         }
     }
-        
+	
+	// Guarda el valor de la localizacion enun espacio vacio de la lista
     private void guardarLocalizacion(Scanner scanner, Localizacion lLocalizacion[]) throws GestorArchivosException
     {
-    	String nombreDeSala = scanner.next();
+		String nombreDeSala = scanner.next();
+		// Se recorre el array de localizaciones ...
     	for (int i = 0; i <= lLocalizacion.length; i++) {
+			// ... y cuando encontramos una posicion vacia ...
     		if (lLocalizacion[i] == null) {
+				// ... crea una nueva sala y la guarda en esa posicion.
     			lLocalizacion[i] = new Localizacion(nombreDeSala);
-    			//Contar el numero de localizacione sdayacentes que tiene.
+    			//Aprobechamos para contar el numero de elementos que acompañan al nombre de la sala...
     			int cont = 0;
     			while (scanner.hasNext()){
     				cont++;
                     scanner.next();
-                }
+				}
+				//... para crear el array de localizaciones adyacentes.
     			lLocalizacion[i].setLocalizacionesAdyacentes(new Localizacion[cont]);
     			break;
-    		}
+			}
+			// Si encontramos el nombre de la sala una segunda vez lanzamo una excepcion para informar de que la sala esta repetida.
     		else if (lLocalizacion[i].getNombre() == nombreDeSala) {
     			throw new GestorArchivosException(nLinea,nombreArchivo,"sala repetida");
     		}
     	}
     }
-    
+	
+	// Guarda una sala del anexo en en array lista de personajes
     private void guardarPersonaje(Scanner scanner, Localizacion lLocalizacion[], Personaje lPersonaje[]) throws GestorArchivosException
     {
-    	String nombre = scanner.next();
+		String nombre = scanner.next();
+		// Recorre el array de personajes
     	for (int i = 0; i <= lPersonaje.length; i++) {
+			// Si hay una poscion vacia creamos un nuevo jugador y lo guardamos.
     		if (lPersonaje[i] == null) {
     			Localizacion localizacion = buscarSalaSeguro(scanner.next(), lLocalizacion);
     			lPersonaje[i] = new NPC_aleatorio(nombre, localizacion);
     			break;
-    		}
+			}
+			// Si encontramos el nombre del personaje que queremos guardar lanzmaos una excepcion para informar de que el personaje esta repetido.
     		else if (lPersonaje[i].getNombre() == nombre)
     			throw new GestorArchivosException(nLinea,nombreArchivo,"personaje repetido");
     	}
     }
-    
+	
+	// Guarda un objeto del anexo en en array lista de personajes
     private void guardarObjeto(Scanner scanner, Localizacion lLocalizacion[], Personaje lPersonaje[], Objeto lObjeto[]) throws GestorArchivosException
     {
     	String nombre = scanner.next();
     	for (int i = 0; i <= lObjeto.length; i++) {
+			// Si encontramos una posicion vacia guardamos el objeto
     		if (lObjeto[i] == null) {
-    			// Se crea el objeto
     			lObjeto[i] = new Objeto(nombre);
     			// Se le asigna el objeto al jugador o localizacion corespondiente.
     			nombre = scanner.next();
@@ -229,22 +239,24 @@ public class GestorArchivos {
     				if (personaje != null) {
     					personaje.setObjeto(lObjeto[i]);
         			}
-    				else
+    				else// Si no se encuentra la posicion en el array personajes o localizacion se lanza una excepcion de referencia inexistente.
     					throw new GestorArchivosException(nLinea,nombreArchivo,"referencia inexistente a " + nombre);
     			}
     			
     			break;
-    		}
+			}
+			// Si se encuntra el nombre del objeto que estamos buscando se lanza una excepcion para informar de que el objeto esta repetido.
     		else if (lObjeto[i].getNombre() == nombre)
     			throw new GestorArchivosException(nLinea,nombreArchivo,"objeto repetido");
     	}
     }
-    
+	
+	// Recorre el array de localizaciones por segunnda vez(necesita tener las localizaciones ya creadas) para unir las localizaciones adyacentes.
     private void unirLocalizacionesAdyacentes(Scanner s, Localizacion lLocalizacion[]) throws GestorArchivosException
     {
     	nLinea = 0;
     	selector = 1;
-		int contSalas = 0;
+		int contSalas = 0; // utilizamos este contador para reconocer las salas.
 		int contSalasAdyacentes = 0;
         while (s.hasNextLine()) {
         	nLinea++;
@@ -258,7 +270,7 @@ public class GestorArchivos {
             else if (selector == 0){
             	Scanner sl = new Scanner (linea);
                 sl.useDelimiter("\\s*(\\(|,|\\))\\s*");
-                sl.next();
+                sl.next(); // Se descarta la primera entrada porque es el nombre de la propia sala
                 while (sl.hasNext()){
                 	lLocalizacion[contSalas].getAdyacencias()[contSalasAdyacentes] = buscarSalaSeguro(sl.next(), lLocalizacion);
                 	contSalasAdyacentes++;
@@ -269,7 +281,8 @@ public class GestorArchivos {
             }
         }
     }
-    
+	
+	// Lee el AnexoII y guarda los valores en la los arrays.
     private void darValoresAnexoII(Scanner s, Personaje lPersonaje[]) throws GestorArchivosException
     {
     	nLinea = 0;
@@ -305,19 +318,20 @@ public class GestorArchivos {
 			personaje.setObjetivo(new Ubicacion(scanner.next()));
     }
     
-  //Se el pasa el scanner de un objeto objetivo y se lo guarda al personaje.
+  	//Se el pasa el scanner de un objeto objetivo y se lo guarda al personaje.
     private void guardarObjetoObjetivo(Scanner scanner, Personaje lPersonaje[]) throws GestorArchivosException {
     	String objeto = scanner.next();
-    	Personaje personaje = null;
-    	
+		
+		// Se comprueba el objeto tiene despues un personaje alque ser asignado.
     	if (scanner.hasNext()) {
-    		personaje = buscarPersonajeSeguro(scanner.next(), lPersonaje);
+    		Personaje personaje = buscarPersonajeSeguro(scanner.next(), lPersonaje);
         	
         	if (personaje != null)
         		personaje.getObjetivo().setNombre(objeto);
     	}
     }
-    
+	
+	// Crea las creencias vacias de cada jugador, guardando los nombres de todos los jugadores y las salas.
     private void crearCreencias(Personaje lPersonaje[], Objeto lObjeto[]) {
     	for(int i=0; i<lPersonaje.length; ++i) {
     		// Se crea la lista de creencias indicando el tamaño de las listas
@@ -334,7 +348,8 @@ public class GestorArchivos {
     	}
     }
     
-    //Funciones Buscar
+	//Funciones Buscar
+	// Busca una sala sin lanzar excepcion.
     private Localizacion buscarSala(String nombre, Localizacion lLocalizacion[]) {
     	Localizacion localizacion = null;
     	for(int i = 0; i < lLocalizacion.length; i++) {
@@ -346,7 +361,8 @@ public class GestorArchivos {
     	
     	return localizacion;
     }
-    
+	
+	// La misma funcion buscr sala pero lanzando una excepcion de referencia inexistente
     private Localizacion buscarSalaSeguro(String nombre, Localizacion lLocalizacion[]) throws GestorArchivosException
     {
     	Localizacion localizacion = buscarSala(nombre, lLocalizacion);
@@ -354,7 +370,8 @@ public class GestorArchivos {
     		throw new GestorArchivosException(nLinea,nombreArchivo,"referencia inexistente a " + nombre);
     	return localizacion;
     }
-    
+	
+	// Funcion buscar personaje
     private Personaje buscarPersonaje(String nombre, Personaje lPersonaje[]) {
     	Personaje personaje = null;
     	for(int i = 0; i < lPersonaje.length; i++) {
@@ -366,7 +383,8 @@ public class GestorArchivos {
     	
     	return personaje;
     }
-    
+	
+	// Funcion buscar presonaje pero lanzando una excepcion de referencia inexistente si no se encuentra.
     private Personaje buscarPersonajeSeguro(String nombre, Personaje lPersonaje[]) throws GestorArchivosException
     {
     	Personaje personaje = buscarPersonaje(nombre, lPersonaje);
