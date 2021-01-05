b6d062dd94cb385a5e2193dca02507dc240a1b1f
aparpo <alvaro.parpolowicz@live.u-tad.com>
2021-01-05 19:01:17 +0100

    Nueva clase main
    Clase objeto depurada

--- ProyectoPOO/src/Objeto.java
+++ ProyectoPOO/src/Objeto.java
@@ -1,51 +1,18 @@
 
 public class Objeto {
     private String nombre;
-    //los objetos pueden estar cogidos por algun personaje o sueltos
-    /*private char estado;*/
-    //al crear el primer objeto esta variable se pondra a 0
-    private static int numObjetos = 0;
-
-    //contar el numero de objetos creados
-    static void nuevoObjeto(){
-        numObjetos++; //se incrementa cada vez que creamos un objeto
-    }
-
-    //constructor sin parametros
-    Objeto() {
-        nombre = "";
-        nuevoObjeto();
-    }
 
     //constructor con parametros
     Objeto(String n){
         nombre = n;
-        nuevoObjeto();
     }
 
-    //set
     public void setNombre(String txt){
         nombre = txt;
     }
 
-    /*public void setEstado(char j){
-        estado = j;
-    }*/
-
-    //Get
     public String getNombre(){
         return nombre;
     }
 
-    /*public String getEstado(){
-        String s;
-        if (estado == 'c') {s = "Cogido por algun personaje";}
-        else if (estado == 's') {s = "Suelto por el mapa";}
-        else {s = "El estado del objeto es desconodido";}
-        return s;
-    }*/
-
-    //dar objeto
-    //coger objeto
-    //soltar objeto
 }
